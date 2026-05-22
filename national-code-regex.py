182d979c2a66398408905bb5bb1d82773ff5d5dd
RafaelHades <131418449+RafaelHades@users.noreply.github.com>
2026-05-22 21:42:45 +0330

    Update national-code-regex.py

--- national-code-regex.py
+++ national-code-regex.py
@@ -1,6 +1,22 @@
 import re
 
-NATIONAL_CODE_REGEX = r'^(?!0{10})[1-9]\d{9}$'
-
 def validate_national_code(code):
-    return bool(re.match(NATIONAL_CODE_REGEX, code))
+    code = str(code).strip()
+    
+    if not code.isdigit() or len(code) != 10:
+        return False
+    
+    if len(set(code)) == 1:
+        return False
+    
+    sum = 0
+    for i in range(9):
+        sum += int(code[i]) * (10 - i)
+    
+    remainder = sum % 11
+    check_digit = int(code[9])
+    
+    if remainder < 2:
+        return check_digit == remainder
+    else:
+        return check_digit == 11 - remainder
